0ef852705785568ac8d8869c754057b07bf522f0
‎ <>
2025-03-01 23:13:37 +0700

    update

--- test/general_developer_action_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:general_developer_action/general_developer_action.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
